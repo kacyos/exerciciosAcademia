6623274a779757aaf3e80504b1608c1b2d754356
Cacio de Castro <cacio.castro@gmail.com>
2023-04-04 15:26:54 -0300

    Add: Exercicio para receber dados de produtos e exibir total com IPI

---
+++ CalcularValorDeProdutos/CalcularValorDeProdutos/Program.cs
@@ -0,0 +1,33 @@
+﻿namespace CalcularValorDeProdutos {
+    internal class Program {
+        static void Main(string[] args) {
+            double valuePoductA;
+            string codProductA;
+            int qtdPoductA;
+
+            double valuepoductB;
+            string codProductB;
+            int qtdPoductB;
+
+            Console.Write("Digite o código do produto A: ");
+            codProductA = Console.ReadLine();
+            Console.Write("\nDigite a quantidade: ");
+            qtdPoductA = int.Parse(Console.ReadLine());
+            Console.Write("\nDigite o valor do produto A: ");
+            valuePoductA = double.Parse(Console.ReadLine());
+
+            Console.Write("\n\nDigite o código do produto B: ");
+            codProductB = Console.ReadLine();
+            Console.Write("\nDigite a quantidade: ");
+            qtdPoductB = int.Parse(Console.ReadLine());
+            Console.Write("\nDigite o valor do produto B: ");
+            valuepoductB = double.Parse(Console.ReadLine());
+            
+            double sum = (valuePoductA * qtdPoductA) + (valuepoductB * qtdPoductB);
+            double percent = sum * 0.3;
+
+            Console.WriteLine($"O valor total com IPI de 30% é: {sum + percent}");
+
+        }
+}
+}
