747dec48261178c96f09956cdfdee1d862a2a1a3
Cacio de Castro <cacio.castro@gmail.com>
2023-04-04 15:51:51 -0300

    add: Calcular salário do vendedor

---
+++ CalculoDeVendas/CalculoDeVendas/Program.cs
@@ -0,0 +1,26 @@
+﻿namespace CalculoDeVendas {
+    internal class Program {
+        static void Main(string[] args) {
+
+            int sellerNumber;
+            double salary, percentage, totalSalary, salesAmount;
+
+            Console.Write("Escreva o número do vendedor: ");
+            sellerNumber = int.Parse(Console.ReadLine());
+
+            Console.Write("Escreva o total de vendas: ");
+            salesAmount = int.Parse(Console.ReadLine());
+
+            Console.Write("Escreva o salário fixo do vendedor: ");
+            salary = double.Parse(Console.ReadLine());
+
+            Console.Write("Escreva o percentual sobre as vendas: ");
+            percentage = double.Parse(Console.ReadLine());
+
+            totalSalary = salary + (salesAmount * (percentage / 100));
+
+            Console.WriteLine($"\n\nO Salário do vendedor nº {sellerNumber} é R$ {totalSalary}");
+
+        }
+    }
+}
