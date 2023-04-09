6cef22a983932d38285227ad3e60979789d11def
Cacio de Castro <cacio.castro@gmail.com>
2023-04-09 17:21:27 -0300

    finalização dos exercicios

---
+++ 02_Exercícios_Seleção/14CalculoDeDownload/14CalculoDeDownload/Program.cs
@@ -0,0 +1,15 @@
+﻿namespace _14CalculoDeDownload {
+    internal class Program {
+        static void Main(string[] args) {
+            Console.Write("Informe o tamanho do arquivo: ");
+            double tamanhoDoArquivo = double.Parse(Console.ReadLine());
+
+            Console.Write("Informe a velocidade do Link da internet: ");
+            double velocidadeInternet = double.Parse(Console.ReadLine());
+
+            double tempoEstimado = ((tamanhoDoArquivo/ velocidadeInternet / 8) / 60) ;
+
+            Console.WriteLine($"O tempo estimado de download em minutos é: {(int) tempoEstimado} Minutos");
+        }
+    }
+}
